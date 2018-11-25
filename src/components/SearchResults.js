08596017e767288b623587eaef6c080c5b3a4b1f
vikas kumar <vikas@simplotel.com>
2018-11-25 19:11:11 +0530

    making SearchVideoMeta component a functional component

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import SearchVideoMeta from "./SearchVideoMeta";
 import getResults from "../actionCreators/setResults";
+import changeVideoFunction from "../actionCreators/changeVideo";
+import setPlayListFunction from "../actionCreators/editPlaylist";
 
 class SearchResults extends React.Component {
   componentDidMount() {
@@ -18,6 +20,8 @@ class SearchResults extends React.Component {
               thumbnail={video.thumbnail}
               title={video.title}
               description={video.description}
+              handleVideoChange={this.props.handleVideoChange}
+              addVideoToPlaylist={this.props.addVideoToPlaylist}
             />
           );
         })}
@@ -32,6 +36,14 @@ const mapStateToProps = ({ query, results }) => ({
 const mapDispatchToProps = dispatch => ({
   search() {
     dispatch(getResults());
+  },
+  handleVideoChange(event) {
+    event.stopPropagation();
+    dispatch(changeVideoFunction(event.target.dataset.id));
+  },
+  addVideoToPlaylist(event) {
+    let data = JSON.parse(JSON.stringify(event.currentTarget.dataset));
+    dispatch(setPlayListFunction(data));
   }
 });
 export default connect(
--- src/components/SearchVideoMeta.js
+++ src/components/SearchVideoMeta.js
@@ -1,52 +1,30 @@
 import React from "react";
-import { connect } from "react-redux";
 import playIcon from "../images/play-icon2.gif";
-import changeVideo from "../actionCreators/changeVideo";
-import setPlaylist from "../actionCreators/editPlaylist";
 
-class SearchVideoMeta extends React.Component {
-  render() {
-    const { thumbnail, title, description, id } = this.props;
-    return (
-      <div
-        className="video"
-        onClick={this.props.addVideoToPlaylist}
-        data-id={id}
-        data-title={title}
-      >
-        {thumbnail ? (
-          <img className="video-image" src={thumbnail} alt="video " />
-        ) : null}
-        <p className="video-title">{title}</p>
-        {description ? (
-          <p className="video-description">{description}</p>
-        ) : null}
+export default function SearchVideoMeta(props) {
+  const { thumbnail, title, description, id } = props;
 
-        {thumbnail ? (
-          <img
-            src={playIcon}
-            className="playVideo"
-            alt="play video"
-            onClick={this.props.handleVideoChange}
-            data-id={id}
-          />
-        ) : null}
-      </div>
-    );
-  }
+  return (
+    <div
+      className="video"
+      onClick={props.addVideoToPlaylist}
+      data-id={id}
+      data-title={title}
+    >
+      {thumbnail ? (
+        <img className="video-image" src={thumbnail} alt="video " />
+      ) : null}
+      <p className="video-title">{title}</p>
+      {description ? <p className="video-description">{description}</p> : null}
+      {thumbnail ? (
+        <img
+          src={playIcon}
+          className="playVideo"
+          alt="play video"
+          onClick={props.handleVideoChange}
+          data-id={id}
+        />
+      ) : null}
+    </div>
+  );
 }
-const mapDispatchToProps = dispatch => ({
-  handleVideoChange(event) {
-    event.stopPropagation();
-    dispatch(changeVideo(event.target.dataset.id));
-  },
-  addVideoToPlaylist(event) {
-    let data = JSON.parse(JSON.stringify(event.currentTarget.dataset));
-    dispatch(setPlaylist(data));
-  }
-});
-
-export default connect(
-  "",
-  mapDispatchToProps
-)(SearchVideoMeta);
